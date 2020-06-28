1c661d11bf42417fa6bbeaa7e964e49be07d26e7
Corie Lynch <Lynchc17@gmail.com>
2020-06-28 15:15:19 -0700

    Set up folders files and connections

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger with Egg');
+
+INSERT INTO burgers (burger_name) VALUES ('Buffalo burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Mushroom burger');
